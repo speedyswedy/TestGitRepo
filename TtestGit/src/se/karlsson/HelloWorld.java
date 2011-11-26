ada0ef85bf6097e65b9aa44dd831751182a386f4
Pelle Karlsson <speedyswedy@hotmail.com>
2011-11-26 21:32:55 +0100

    En Annan branch

--- TtestGit/src/se/karlsson/HelloWorld.java
+++ TtestGit/src/se/karlsson/HelloWorld.java
@@ -3,5 +3,6 @@ package se.karlsson;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Kallesss");
+        System.out.println("En Annan");
     }
 }
